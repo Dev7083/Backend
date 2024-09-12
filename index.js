6cb7c53439187011d4041ae744b9abd591248f10
dev7083 <113914000+Dev7083@users.noreply.github.com>
2024-09-12 18:43:08 +0530

    noob backend server

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("I'm Batman");
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('GeekyDev_')
+})
+
+app.get('/night', (req, res) => {
+    res.send(' <h1> Hey Batman It\'s Night. Let\'s Save the Gotham.</h1>')
+}
+)
+
+app.get('/day', (req, res) => {
+    res.send(' <h1> Hey Batman It\'s Day. Let\'s sleep we\'ve to save the city during night.</h1>')
+}
+)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
